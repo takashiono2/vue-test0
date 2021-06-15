e66ad0b6abcf284640c059f26cf06d8b035d83f3
takashiono2 <takashiono710@gmail.com>
2021-06-15 23:28:40 +0900

    todoの削除機能

--- js/main.js
+++ js/main.js
@@ -8,18 +8,24 @@
         'task1',
         'task2',
         'task3'
-    ]},
+      ]
+    },
       // methods: {
       //   addItem: function(e){
       //     e.preventDefault();
       //     this.todos.push(this.newItem);
       //   }
       // }
-      methods: {
-        addItem: function(){
-          this.todos.push(this.newItem);
-          this.newItem ='';
+    methods: {
+      addItem: function(){
+        this.todos.push(this.newItem);
+        this.newItem ='';
+      },
+      deleteItem: function(index){
+        if(confirm('are you sure?')){
+          this.todos.splice(index,1);
         }
       }
+    }
   });
 })();
