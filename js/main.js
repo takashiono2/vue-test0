50fbea595b2f96d7caf898bc3b72ad8dcbc98e0d
takashiono2 <takashiono710@gmail.com>
2021-06-17 00:03:42 +0900

    props値

--- js/main.js
+++ js/main.js
@@ -2,12 +2,19 @@
   'use strict';
 
   var likeComponent = Vue.extend({
+    // props: ['message'],
+    props:{
+      message:{ 
+        type: String,
+        default: 'Like'
+      }
+    },
     data: function(){
       return{
         count:0
       }
     },
-    template: '<button @click="countUp">Like {{ count }}</button>',
+    template: '<button @click="countUp">{{ message }} {{ count }}</button>',
     methods: {
       countUp: function(){
         this.count++;
