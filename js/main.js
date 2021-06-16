23c4a7c56ed57cdb862a11b061c731678078b17a
takashiono2 <takashiono710@gmail.com>
2021-06-16 18:57:31 +0900

    LocalStorageを使う

--- js/main.js
+++ js/main.js
@@ -15,6 +15,19 @@
         isDone: true
       }]
     },
+    watch:{
+      // todos: function(){
+      //   localStorage.setItem('todos',JSON.stringify(this.todos));
+      //   alert('Data saved!');
+      // }
+      todos:{ 
+        handler: function(){
+          localStorage.setItem('todos',JSON.stringify(this.todos));
+          alert('Data saved!');
+        },
+      deep: true
+      }
+    },
     methods: {
       addItem: function(){
         var item = { 
