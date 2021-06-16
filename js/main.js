6fba03910c8ab5d5207c84abda4a76f00bd2d51c
takashiono2 <takashiono710@gmail.com>
2021-06-16 22:49:46 +0900

    component

--- js/main.js
+++ js/main.js
@@ -1,54 +1,68 @@
 (function(){
   'use strict';
-  var vm = new Vue({
+  
+  var likeComponent = Vue.extend({
+    template: '<button>Like</button>'
+  });
+
+  var app = new Vue({
     el: '#app',
-    data:{ 
-      newItem:'',
-      todos:[]
-    },
-    watch:{
-      todos:{ 
-        handler: function(){
-          localStorage.setItem('todos',JSON.stringify(this.todos));
-        },
-      deep: true
-      }
-    },
-    mounted: function(){
-      this.todos = JSON.parse(localStorage.getItem('todos')) || [];
-    },
-    methods: {
-      addItem: function(){
-        var item = { 
-          title: this.newItem,
-          isDone: false
-        };
-        this.todos.push(item);
-        this.newItem ='';
-      },
-      deleteItem: function(index){
-        if(confirm('are you sure?')){
-          this.todos.splice(index,1);
-        }
-      },
-      purge: function(){
-        if(!confirm('delete finished')){
-          return;
-        }
-        // this.todos = this.todos.filter(function(todo){
-        //     return !todo.isDone;
-        this.todos = this.remaining;
-      }
-    },
-    computed: {
-      remaining: function(){
-        // var items = this.todos.filter(function(todo){
-        //   return !todo.isDone;
-        // });
-        return this.todos.filter(function(todo){
-          return !todo.isDone;  
-        });
-      }
+    components: {
+      'like-component': likeComponent
     }
   });
 })();
+// (function(){
+//   'use strict';
+//   var vm = new Vue({
+//     el: '#app',
+//     data:{ 
+//       newItem:'',
+//       todos:[]
+//     },
+//     watch:{
+//       todos:{ 
+//         handler: function(){
+//           localStorage.setItem('todos',JSON.stringify(this.todos));
+//         },
+//       deep: true
+//       }
+//     },
+//     mounted: function(){
+//       this.todos = JSON.parse(localStorage.getItem('todos')) || [];
+//     },
+//     methods: {
+//       addItem: function(){
+//         var item = { 
+//           title: this.newItem,
+//           isDone: false
+//         };
+//         this.todos.push(item);
+//         this.newItem ='';
+//       },
+//       deleteItem: function(index){
+//         if(confirm('are you sure?')){
+//           this.todos.splice(index,1);
+//         }
+//       },
+//       purge: function(){
+//         if(!confirm('delete finished')){
+//           return;
+//         }
+//         // this.todos = this.todos.filter(function(todo){
+//         //     return !todo.isDone;
+//         this.todos = this.remaining;
+//       }
+//     },
+//     computed: {
+//       remaining: function(){
+//         // var items = this.todos.filter(function(todo){
+//         //   return !todo.isDone;
+//         // });
+//         return this.todos.filter(function(todo){
+//           return !todo.isDone;  
+//         });
+//       }
+//     }
+//   });
+// })();
